f8a7713deb2fb0b12f9435ef2a6e23a96cab82b6
vikxzzzz <162162844+vikxzzzz@users.noreply.github.com>
2024-05-12 23:27:19 -0300

    AC9.py

--- AC9.py
+++ AC9.py
@@ -57,14 +57,56 @@ print(numeros.count(T))
 
 #BEECROWD 2339
 
-C = int(input())
-P = int(input())
-F = int(input())
-
-if P >= C * F:
+C, P, F = (input().split())
+if int(C) * int(F) <= int(P):
     print('S')
 else:
     print('N')
+
+
+
+
+#BEECROWD 2388
+
+N = int(input())
+distancia = 0
+
+for i in range(N):
+    t, v = map(int, input().split())
+    distancia += t * v
+
+print(distancia)
+
+
+
+#BEECROWD 2413
+
+t = int(input())
+
+clicks = t * 4
+
+print(clicks)
+
+
+
+#BEECROWD 3084
+
+N = int(input())
+sequencia = [int(input()) for _ in range(N)]
+
+x = [[0, 0] for _ in range(N+1)]
+x[0][0] = 0
+x[0][1] = 0
+
+for i in range(1, N + 1):
+    if sequencia[i - 1] == 1:
+        x[i][0] = max(x[i - 1][0], x[i - 1][1])
+        x[i][1] = x[i - 1][0] + 1
+    else:
+        x[i][1] = x[i - 1][0] + 1
+
+print(max(x[N][0], x[N][1]))
+
 
 
 
